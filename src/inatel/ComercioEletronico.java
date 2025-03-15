79084ad4ff22b3cd184f53c7434198f6e6c15d0a
José Enderson Ferreira Rodrigues <jose.e.f.rodrigues.br@gmail.com>
2025-03-15 19:47:09 -0300

    -

--- src/inatel/ComercioEletronico.java
+++ src/inatel/ComercioEletronico.java
@@ -17,12 +17,14 @@ public class ComercioEletronico
 		Produto produto3 = new Produto("Sofá", 700.86, categoria3);
 		Produto produto4 = new Produto("Geladeira", 1558.35, categoria2);
 		// Pedido1
+		// Pagamento utilizando modalidade de pagamento via cartão e modelo de fatura enviada por email
 		Pedido pedido1 = new Pedido();
 		pedido1.adicionarProduto(produto1);
 		pedido1.adicionarProduto(produto2);
 		pedido1.adicionarProduto(produto3);
 		pedido1.finalizar(new PagamentoCartaoCredito("8888-2222-4444-3333"), new FaturaEmail("teste@inatel.com.br"));
 		// Pedido2
+		// Pagamento utilizando modalidade PIX e modelo de fatura enviada para impressora
 		Pedido pedido2 = new Pedido();
 		pedido2.adicionarProduto(produto3);
 		pedido2.adicionarProduto(produto4);
